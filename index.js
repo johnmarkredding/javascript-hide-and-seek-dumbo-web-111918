7949cf073eda57f3cadfd070ac406dc574f7b080
John Mark Redding <johnmarkredding@gmail.com>
2019-01-10 14:27:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested").querySelector(".target");
+}
+
+function increaseRankBy(n) {
+  let lis = Array.from(document.querySelectorAll(".ranked-list li"));
+  lis.forEach(function (li) {
+    console.log(li);
+    li.innerHTML = parseInt(li.innerHTML) + n;
+  });
+}
+
+function deepestChild() {
+  let grandNode = document.querySelector("#grand-node");
+  console.log(grandNode);
+  
+}
+
+deepestChild();
