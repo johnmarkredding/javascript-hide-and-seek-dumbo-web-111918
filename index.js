77f3ef232325f3e7624e931973cafb6aa725b48e
John Mark Redding <johnmarkredding@gmail.com>
2019-01-10 14:28:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  let grandNode = document.querySelector("#grand-node");
+  let grandNode = document.querySelector("#grand-node div");
   console.log(grandNode);
 }
 
